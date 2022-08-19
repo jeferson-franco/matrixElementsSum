28e4280271c8c859c4e50d95bfd2729e04be18ec
jefersonfranco <jefersonfranco@protonmail.com>
2022-08-19 19:34:51 -0300

    update matrixElementsSum.js

--- src/matrixElementsSum.js
+++ src/matrixElementsSum.js
@@ -1,14 +1,14 @@
 function solution(matrix) {
     let s = 0;
-    for (var i = 0; i < matrix.length; i++) {
-        for (var j = 0; j < matrix[0].length; j++) {
-            if (matrix[i][j] == 0 && i+1 < matrix.length) {
-                matrix[i+1][j] = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] == 0 && i + 1 < matrix.length) {
+                matrix[i + 1][j] = 0;
             }
-            s += matrix[i][j]
+            s += matrix[i][j];
         }
     }
-    return s
+    return s;
 }
 
 module.exports = solution;
@@ -17,3 +17,28 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test matrixElementsSum
+
+// alternative solution
+// function solution(matrix) {
+//     for (let r = 0; j = 0; j < matrix[0].length; j++) {
+//         for (let i = 0; i < matrix.length; i++) {
+//             if (matrix[i][j] == 0) break
+//             else r += matrix[i][j]
+//         }
+//     }
+//     return r
+// }
+
+// alternative solution
+// function solution(matrix) {
+//     let total = 0;
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[0].length; j++) {
+//             if (matrix[i][j] == 0) {
+//                 break;
+//             }
+//             total += matrix[i][j];
+//         }
+//     }
+//     return total;
+// }
